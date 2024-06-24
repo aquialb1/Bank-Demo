06619e120588fbcc6a0bdf42a32eee950bccee6e
Aquialb <aquialb@gmail.com>
2024-06-24 16:11:43 -0400

    Chnaged all recursion calls --> do/while loops

--- Main.cpp
+++ Main.cpp
@@ -44,24 +44,26 @@ void account_entry() {
 	cout << user_account.checking << user_account.checking_value << endl;
 	cout << user_account.savings << user_account.savings_value << endl << endl;
 
-	cout << "1 - Deposit  |  2 - Withdraw  |  3 - Back" << endl << endl;
-	cout << "Selection: ";
-	cin >> selection;
-	cout << endl;
+	do {
+		cout << "1 - Deposit  |  2 - Withdraw  |  3 - Back" << endl << endl;
+		cout << "Selection: ";
+		cin >> selection;
+		cout << endl;
 
-	if (selection == "1") {
-		deposit();
-	}
-	else if (selection == "2") {
-		cout << "Withdraw";
-	}
-	else if (selection == "3") {
-		main();
-	}
-	else {
-		cout << "Invalid selection. Please try again." << endl << endl;
-		account_entry();
-	}
+		if (selection == "1") {
+			// deposit();
+			cout << "Deposit" << endl;
+		}
+		else if (selection == "2") {
+			cout << "Withdraw" << endl;
+		}
+		else if (selection == "3") {
+			main();
+		}
+		else {
+			cout << "Invalid selection. Please try again." << endl << endl;
+		}
+	} while (selection != "3");
 }
 
 void returning_user() {
@@ -73,20 +75,21 @@ void returning_user() {
 	cout << "--------------------" << endl << endl;
 	cout << "Returning User" << endl << endl;
 
-	cout << "Username: ";
-	cin >> username_input;
+	do {
+		cout << "Username: ";
+		cin >> username_input;
 
-	cout << "Password: ";
-	cin >> password_input;
-	cout << endl;
+		cout << "Password: ";
+		cin >> password_input;
+		cout << endl;
 
-	if (username_input == user_account.username && password_input == user_account.password) {
-		account_entry();
-	}
-	else {
-		cout << "The entered username or password is incorrect. Please try again." << endl << endl;
-		returning_user();
-	}
+		if (username_input == user_account.username && password_input == user_account.password) {
+			account_entry();
+		}
+		else {
+			cout << "The entered username or password is incorrect. Please try again." << endl << endl;
+		}
+	} while (username_input != user_account.username && password_input != user_account.password);
 }
 
 int main()
@@ -111,7 +114,6 @@ int main()
 		}
 		else {
 			cout << "Invalid selection. Please try again." << endl << endl;
-			main();
 		}
 	} while (selection != "2");
 
